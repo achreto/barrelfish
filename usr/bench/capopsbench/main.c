ce743b5be2bfd8206ce3ed7950e278b8a27565a4
Reto Achermann <reto.achermann@inf.ethz.ch>
2020-12-05 01:25:13 +0100

    bench/capos: fix csv output
    Signed-off-by: Reto Achermann <reto.achermann@inf.ethz.ch>

--- usr/bench/capopsbench/main.c
+++ usr/bench/capopsbench/main.c
@@ -251,7 +251,7 @@ int main(int argc, char *argv[])
 
     size_t maxcores = ncores;
     for (ncores = 0; ncores <= maxcores; ncores++) {
-        printf("NCORES=%zu,", ncores);
+        printf("NCORES=%zu, ", ncores);
         printf("NROUND=%zu,", nrounds);
         cycles_t sum = 0;
         for (size_t i = 0; i < nrounds; i++) {
