4c746204801792fbccd4713d262672b1efd5385a
Rishika Varma K <rishikavarmak@gmail.com>
2025-07-28 18:06:32 +0200

    Setup barrelfish build system for tests

--- kernel/arch/x86_64/paging_test_utils.c
+++ kernel/arch/x86_64/paging_test_utils.c
@@ -1,5 +1,35 @@
 #include <kernel/arch/x86_64/paging_test_utils.h>
 
+#include <kernel.h>
+#include <string.h>
+#include <stdio.h>
+#include <paging_kernel_arch.h>
+#include <elf/elf.h>
+#include <init.h>
+#include <irq.h>
+#include <x86.h>
+#include <serial.h>
+#include <kernel_multiboot.h>
+#include <syscall.h>
+#include <getopt/getopt.h>
+#include <exec.h>
+#include <kputchar.h>
+#include <systime.h>
+#include <arch/x86/conio.h>
+#include <arch/x86/pic.h>
+#include <arch/x86/apic.h>
+#include <arch/x86/mcheck.h>
+#include <arch/x86/perfmon.h>
+#include <arch/x86/rtc.h>
+#include <target/x86/barrelfish_kpi/coredata_target.h>
+#include <arch/x86/timing.h>
+#include <arch/x86/start_aps.h>
+#include <arch/x86/ipi_notify.h>
+#include <barrelfish_kpi/cpu_arch.h>
+#include <target/x86_64/barrelfish_kpi/cpu_target.h>
+#include <coreboot.h>
+#include <kcb.h>
+
 void write_pte(lpaddr_t source, lpaddr_t dest, bool valid){
 }
 
--- kernel/arch/x86_64/trace_test.c
+++ kernel/arch/x86_64/trace_test.c
@@ -1,20 +1,22 @@
 #include <kernel/arch/x86_64/trace_test.h>
+#include <kernel/arch/x86_64/paging_test_utils.h>
+#include <kernel/arch/x86_64/apic.h>
 
 void execute_test(void){
     if(apic_id==0){
-        write_pte(0x3008, 0x5000, true);
-        read_memory(0x200000);
-        write_pte(0x6000, 0x1000, true);
-    }else if(apic_id==1){
-        write_pte(0x4000, 0x0, true);
         invalidate_page(0x1000);
-    }else if(apic_id==2){
+        invalidate_page(0x201000);
         write_pte(0x3000, 0x5000, true);
-        write_pte(0x3000, 0x4000, true);
-        write_pte(0x3008, 0x6000, true);
-        read_memory(0x1000);
-        invalidate_page(0x1000);
+        write_pte(0x4000, 0x4000, false);
+        barrier();
+    }else if(apic_id==1){
+        write_pte(0x3008, 0x4000, true);
+        write_pte(0x3000, 0x3000, false);
         write_pte(0x3008, 0x3008, false);
+    }else if(apic_id==2){
+        write_pte(0x3000, 0x6000, true);
+        read_memory(0x201000);
+        read_memory(0x200000);
     }
 }
 
