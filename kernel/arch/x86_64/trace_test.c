99e426399b992e5261f099e6fc8c5608f5cc62c0
Rishika Varma K <rishikavarmak@gmail.com>
2025-07-28 18:18:15 +0200

    Missed files

--- kernel/arch/x86_64/trace_test.c
+++ kernel/arch/x86_64/trace_test.c
@@ -1,22 +1,20 @@
 #include <kernel/arch/x86_64/trace_test.h>
-#include <kernel/arch/x86_64/paging_test_utils.h>
-#include <kernel/arch/x86_64/apic.h>
 
 void execute_test(void){
     if(apic_id==0){
-        invalidate_page(0x1000);
-        invalidate_page(0x201000);
-        write_pte(0x3000, 0x5000, true);
-        write_pte(0x4000, 0x4000, false);
         barrier();
+        invalidate_page(0x201000);
+        barrier();
+        read_memory(0x1000);
     }else if(apic_id==1){
+        invalidate_page(0x201000);
+        invalidate_page(0x0);
+        write_pte(0x4000, 0x1000, true);
         write_pte(0x3008, 0x4000, true);
-        write_pte(0x3000, 0x3000, false);
-        write_pte(0x3008, 0x3008, false);
+        write_pte(0x4000, 0x4000, false);
     }else if(apic_id==2){
-        write_pte(0x3000, 0x6000, true);
-        read_memory(0x201000);
-        read_memory(0x200000);
+        write_pte(0x0, 0x0, false);
+        read_memory(0x0);
     }
 }
 
--- kernel/include/arch/x86_64/trace_test.h.c
+++
@@ -1,7 +0,0 @@
-#ifndef TRACE_TEST_H
-#define TRACE_TEST_H
-
-void execute_test(void);
-
-
-#endif // TRACE_TEST_H
