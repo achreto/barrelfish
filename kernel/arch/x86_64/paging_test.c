699ff016e47830279dfa8dbee4e2ccf576702254
root <root@pinky20.mpi-sws.org>
2025-08-04 20:21:00 +0200

    minor cleanup

--- kernel/arch/x86_64/paging_test.c
+++ kernel/arch/x86_64/paging_test.c
@@ -6,10 +6,10 @@
 #include <arch/x86/apic.h>
 
 void execute_test(void* pd_start, void* pt_start, void* data_start){
+    lpaddr_t pd_phys = mem_to_local_phys((lvaddr_t)pd_start);
+    lpaddr_t pt_phys = mem_to_local_phys((lvaddr_t)pt_start);
+    lpaddr_t data_phys = mem_to_local_phys((lvaddr_t)data_start);
     if(apic_id==1 || apic_id==2 || apic_id==3){
-        lpaddr_t pd_phys = mem_to_local_phys((lvaddr_t)pd_start);
-        lpaddr_t pt_phys = mem_to_local_phys((lvaddr_t)pt_start);
-        lpaddr_t data_phys = mem_to_local_phys((lvaddr_t)data_start);
         write_pte(pd_phys + BASE_PAGE_SIZE*0, 2, 0, pt_phys+BASE_PAGE_SIZE*0, true);
         for (int i = 0; i < 2; i++) {
             // Use virtual addresses that correspond to PML4 index 1
